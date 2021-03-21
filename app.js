2607e43fda465318a8115cd7a2244b8e3b624d2d
yazantafesh <yazantafesh1@gmail.com>
2021-03-21 13:50:30 +0200

    done lab 5

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 var age=prompt("How old are you?");
 
-if(age<18){
+if(age >= 13 && age <= 30){
     alert('Welcome!');
-} else{
-    alert('You are too old, go work!');
+}else if(age < 13 || age > 30 ) {
+    alert('Sorry! you are either too young or too old for this website');
+} else {
+    alert('Please input a number');
 }
 
 var favouriteChamp= prompt("Who's your favourite champion?");
 
+while (favouriteChamp !=='garen' && favouriteChamp !=='caitlyn' && favouriteChamp !=='jinx'){
+favouriteChamp = prompt('Please choose between garen, caitlyn and jinx');
+
+}
+
+var numberOfImages = prompt('How many of them do you want to see?');
+
+var image ='';
+var result ='';
+
+
 
 if (favouriteChamp=='garen'){
 
-    document.write('<img src="../Photos/Garen_0.jpg"/>');
+    image = '<img src="Garen_0.jpg"/>';
 } else if(favouriteChamp=='caitlyn'){
-    document.write('<img src="../Photos/Caitlyn_0.jpg"/>');
+    image ='<img src="Caitlyn_0.jpg"/>';
 } else if(favouriteChamp=='jinx'){
-    document.write('<img src="../Photos/Jinx_0.jpg"/>');
-} else{
-    alert('Please choose between garen, caitlyn or jix');
+    image ='<img src="/Jinx_0.jpg"/>';
+} 
+
+for (var i = 0; i<numberOfImages; i++){
+    result += image;
 }
 
+
+document.write(result);
+
 confirm("Press the button if you are not a robot :P");
+
 
 function changeBackground(color) {
     document.body.style.background = color;
